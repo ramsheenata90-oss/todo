73d268ac4533dc8bde6b70c07f4d462e85182933
ramsheenata90-oss <ramsheenata90@gmail.com>
2026-01-20 20:10:50 +0530

    Delete todo directory

--- todo/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Todo
-from .serializers import TodoSerializer
-
-class TodoView(viewsets.ModelViewSet):
-    serializer_class = TodoSerializer
-    queryset = Todo.objects.all()
